9df3a1a2a82424e434dd068ca7c2f458e41cb351
Moin Almin <almin.crypt@gmail.com>
2024-01-07 01:05:03 +0530

    Creating notes, routers, controllers

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,26 @@
 import "dotenv/config";
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import noteRoutes from "./routes/notes";
+import morgan from "morgan";
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.use("/api/notes", noteRoutes);
+
+app.use((req, res, next) => {
+    next(Error("Endpoint not found!"));
 });
 
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+    console.error(error);
+    let errorMessage = "An unknown error occured!";
+    if (error instanceof Error) errorMessage = error.message;
+    res.status(500).json({ error: errorMessage });
+});
 export default app;
